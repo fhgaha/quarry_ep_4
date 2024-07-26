362fdca75c1eb18d56142514d945e80b0c7a6001
fhgaha <antsy1992@gmail.com>
2024-07-26 21:24:16 +0300

    slightly rotate the room so pixels are wiggling a little bit

--- game/rooms/parking_1/room_parking_1.gd
+++ game/rooms/parking_1/room_parking_1.gd
@@ -5,13 +5,16 @@ const Data := preload('room_parking_1_state.gd')
 
 var state: Data = load("res://game/rooms/parking_1/room_parking_1.tres")
 
+var rotate_clockwise := true
+var rot_step := 0.0001
+var rot_lim  := 0.1
+
 
 #region Virtual ####################################################################################
 # What happens when Popochiu loads the room. At this point the room is in the
 # tree but it is not visible
 func _on_room_entered() -> void:
-	E.camera_zoom(1.5*Vector2.ONE, 0.1)
-	pass
+	E.camera_zoom(1.5 * Vector2.ONE, 0.1)
 
 
 # What happens when the room changing transition finishes. At this point the room
@@ -27,5 +30,13 @@ func _on_room_transition_finished() -> void:
 func _on_room_exited() -> void:
 	pass
 
-
 #endregion
+
+func _process(delta: float) -> void:
+	wiggle_pixels()
+
+
+func wiggle_pixels() -> void:
+	rotation_degrees += rot_step if rotate_clockwise else -rot_step
+	if rotation_degrees >= rot_lim || rotation_degrees <= -rot_lim:
+		rotate_clockwise = !rotate_clockwise
