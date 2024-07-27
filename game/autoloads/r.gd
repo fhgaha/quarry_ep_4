4ea6b7b3f347a58de4841c7cd53c3b3d942149c7
fhgaha <antsy1992@gmail.com>
2024-07-28 00:35:37 +0300

    pool room

--- game/autoloads/r.gd
+++ game/autoloads/r.gd
@@ -4,15 +4,18 @@ extends "res://addons/popochiu/engine/interfaces/i_room.gd"
 # classes ----
 const PRParking1 := preload("res://game/rooms/parking_1/room_parking_1.gd")
 const PRTelephoneBooth := preload("res://game/rooms/telephone_booth/room_telephone_booth.gd")
+const PRPoolFirst := preload("res://game/rooms/pool_first/room_pool_first.gd")
 # ---- classes
 
 # nodes ----
 var Parking1: PRParking1 : get = get_Parking1
 var TelephoneBooth: PRTelephoneBooth : get = get_TelephoneBooth
+var PoolFirst: PRPoolFirst : get = get_PoolFirst
 # ---- nodes
 
 # functions ----
 func get_Parking1() -> PRParking1: return get_runtime_room("Parking1")
 func get_TelephoneBooth() -> PRTelephoneBooth: return get_runtime_room("TelephoneBooth")
+func get_PoolFirst() -> PRPoolFirst: return get_runtime_room("PoolFirst")
 # ---- functions
 
---
+++ game/rooms/pool_first/room_pool_first.gd
@@ -0,0 +1,32 @@
+@tool
+extends PopochiuRoom
+
+const Data := preload('room_pool_first_state.gd')
+
+var state: Data = load("res://game/rooms/pool_first/room_pool_first.tres")
+
+
+#region Virtual ####################################################################################
+# What happens when Popochiu loads the room. At this point the room is in the
+# tree but it is not visible
+func _on_room_entered() -> void:
+	C.player = C.MainParking1
+	E.camera_zoom(1.0 * Vector2.ONE, 0.1)
+	RenderingServer.set_default_clear_color(Color.BLACK)
+
+
+# What happens when the room changing transition finishes. At this point the room
+# is visible.
+func _on_room_transition_finished() -> void:
+	# You can use await E.queue([]) to excecute a sequence of instructions
+	pass
+
+
+# What happens before Popochiu unloads the room.
+# At this point, the screen is black, processing is disabled and all characters
+# have been removed from the $Characters node.
+func _on_room_exited() -> void:
+	pass
+
+
+#endregion
